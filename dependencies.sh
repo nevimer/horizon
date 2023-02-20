afe754c639c51386c238ea365c44ee4dc5e1301f
nevimer <foxmail@protonmail.com>
2023-02-19 23:55:09 -0500

    tgui update

--- dependencies.sh
+++ dependencies.sh
@@ -10,11 +10,11 @@ export BYOND_MINOR=1589
 export RUST_G_VERSION=1.2.0
 
 #node version
-export NODE_VERSION=14
-export NODE_VERSION_PRECISE=14.16.1
+export NODE_VERSION=16
+export NODE_VERSION_PRECISE=16.19.1
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.7.1
+export SPACEMAN_DMM_VERSION=suite-1.7.3
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.9.16
