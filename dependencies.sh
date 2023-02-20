cc815642e696b823fecc5fb041d3e3b5d1ffcbb7
nevimer <77420409+nevimer@users.noreply.github.com>
2023-02-19 21:17:21 -0500

    fix node I hope

--- dependencies.sh
+++ dependencies.sh
@@ -12,7 +12,7 @@ export RUST_G_VERSION=0.5.0
 
 #node version
 export NODE_VERSION=16
-export NODE_VERSION_PRECISE=16.14.1
+export NODE_VERSION_PRECISE=16.19.1
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.7.1
